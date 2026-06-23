cdfe4d6096c1a6ae85ad1a3635627b240f275fbd
Jurica153 <juricaruklin@gmail.com>
2026-06-23 09:40:27 +0200

    Popravak putanja u README

--- InsurePartner/obj/Debug/net8.0/InsurePartner.AssemblyInfo.cs
+++ InsurePartner/obj/Debug/net8.0/InsurePartner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsurePartner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a364ae1f6c9fc7b769e277e13cb5912890667b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InsurePartner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InsurePartner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
